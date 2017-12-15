cc12a690792c0043fe55eb1aa4ba2921d0b64648
David Scheuermann <dsmann1212@gmail.com>
2017-12-14 21:45:19 -0600

    Removed static media upload from urls.py

--- personal/urls.py
+++ personal/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls'), name='blog'),
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='landing.html'), name='landing')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
